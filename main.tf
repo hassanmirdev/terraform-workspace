217227183c60b0f53aedaaacc50c3d196d75fbb0
Hassan Mir <59356530+hassanmirdev@users.noreply.github.com>
2025-01-27 13:01:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,24 +1,21 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
 variable "ami" {
   description = "value"
 }
 
 variable "instance_type" {
   description = "value"
-  type = map(string)
+#  type = map(string)
 
-  default = {
-    "dev" = "t2.micro"
-    "stage" = "t2.medium"
-    "prod" = "t2.xlarge"
-  }
+#  default = {
+#    "dev" = "t2.micro"
+#    "stage" = "t2.medium"
+ #   "prod" = "t2.xlarge"
+#  }
 }
 
 module "ec2_instance" {
   source = "./modules/resources"
   ami = var.ami
-  instance_type = lookup(var.instance_type, terraform.workspace, "t2.micro")
+ # instance_type = lookup(var.instance_type, terraform.workspace, "t2.micro")
+   instance_type= var.instance_type
 }
