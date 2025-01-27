3a36119b6ea3c3f4c313b41e20f0d51b5bc03f95
Hassan Mir <59356530+hassanmirdev@users.noreply.github.com>
2025-01-27 13:58:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,3 +4,13 @@ module "ec2_instance" {
  # instance_type = lookup(var.instance_type, terraform.workspace, "t2.micro")
    instance_type= var.instance_type
 }
+terraform {
+backend "s3" {
+bucket = "terraformgithubbucket"
+key = "tf-github/terrafor.tfstate"
+region = "us-east-1"
+encrypt = true
+dynamodb_table = "terraform-lock-table"
+acl = "bucket-owner-full-control" # S3 bucket ACL for access control
+}
+}
