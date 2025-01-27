bf646b2f5f7c6a9ff80c51bd4f5f59606ee778ca
Hassan Mir <59356530+hassanmirdev@users.noreply.github.com>
2025-01-27 13:14:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "ec2_instance" {
-  source = "./modules/resources"
+  source = "./day-1/modules/resources"
   ami = var.ami
  # instance_type = lookup(var.instance_type, terraform.workspace, "t2.micro")
    instance_type= var.instance_type
