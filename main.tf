352a0c86e2e1530b7806c3b15bb1196649f9ef7f
Hassan Mir <59356530+hassanmirdev@users.noreply.github.com>
2025-01-28 09:54:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,30 +1,40 @@
 variable "ami" {
-  description = "value"
+  description = "The AMI to use for the EC2 instance"
 }
 
 variable "instance_type" {
-  description = "value"
-  type = map(string)
+  description = "A map of allowed instance types per workspace"
+  type        = map(string)
 
   default = {
-    "dev" = "t2.nano"
-    "staging" = "t2.nano"
+    "dev"       = "t2.nano"
+    "staging"   = "t2.micro"
     "production" = "t2.medium"
   }
+
+  validation {
+    condition = (
+      terraform.workspace == "dev" && contains(["t2.nano"], var.instance_type[terraform.workspace]) ||
+      terraform.workspace == "staging" && contains(["t3.micro"], var.instance_type[terraform.workspace]) ||
+      terraform.workspace == "production" && contains(["t2.small"], var.instance_type[terraform.workspace])
+    )
+    error_message = "Invalid instance type for workspace ${terraform.workspace}. Please use the allowed values based on your workspace."
+  }
 }
+
 module "ec2_instance" {
-  source = "./day-1/modules/resources"
-  ami = var.ami
-  instance_type = lookup(var.instance_type, terraform.workspace, "t2.micro")
-   # instance_type= var.instance_type
+  source        = "./day-1/modules/resources"
+  ami           = var.ami
+  instance_type = lookup(var.instance_type, terraform.workspace, "t2.nano")
 }
+
 terraform {
-backend "s3" {
-bucket = "terraformgithubbucket"
-key = "tf-github/terrafor.tfstate"
-region = "us-east-1"
-encrypt = true
-dynamodb_table = "terraform-lock-table"
-acl = "bucket-owner-full-control" # S3 bucket ACL for access control
-}
+  backend "s3" {
+    bucket         = "terraformgithubbucket"
+    key            = "tf-github/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "terraform-lock-table"
+    acl            = "bucket-owner-full-control"
+  }
 }
